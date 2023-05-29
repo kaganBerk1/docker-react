b827aa14508d22f30042685c8f6e70ed7ce55f4b
kaganBerk1 <kagan_berk_gencer@hotmail.com>
2023-05-29 17:13:41 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          It is working on ngix server with Docker 🤤🤤🤤
+        </p>
+        <p>
+          Bellisimo!!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

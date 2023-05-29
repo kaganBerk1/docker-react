f109b97c8353d78bdde2c2cd22ae2d11628e0809
kaganBerk1 <kagan_berk_gencer@hotmail.com>
2023-05-29 18:10:32 +0300

    test bug fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,11 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          It is working on ngix server with Docker 🤤🤤🤤
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          Bellisimo!!!
-        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );

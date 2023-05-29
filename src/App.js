35f42dec4324fda377975ed4907ebc9d5beddecb
kaganBerk1 <kagan_berk_gencer@hotmail.com>
2023-05-29 19:50:15 +0300

    added travis deploy keys

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bellisimo !!!
+          Bellisimo !!!  tutrino!!
         </p>
         <a
           className="App-link"
